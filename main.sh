f7e8597ea8771e633ad2bc77c33bbb2544d8f2b6
asafnalbant <asaf8547@gmail.com>
2026-06-01 22:11:52 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Ahmet Asaf Nalbant
 # 2420171035
-# Sertifikalar:
 # 1. https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=/OKMhw1WX4r
 # 2. https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=/0Koh8e0Ggd
 # 3. https://credsverse.com/credentials/c26b662a-3b2a-4a7e-b338-c04725e694ed
@@ -9,17 +8,19 @@
 REPORT="report.log"
 
 
-echo "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" > "$REPORT"
+date -u +"%Y-%m-%dT%H:%M:%SZ" > "$REPORT"
 
 
 echo "--- Donanim Bilgileri ---" >> "$REPORT"
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+   
     wmic cpu get name >> "$REPORT"
     wmic memorychip get capacity >> "$REPORT"
     wmic baseboard get product >> "$REPORT"
     wmic diskdrive get serialnumber >> "$REPORT"
     getmac >> "$REPORT"
 else
+
     system_profiler SPHardwareDataType >> "$REPORT"
     ifconfig >> "$REPORT"
 fi
